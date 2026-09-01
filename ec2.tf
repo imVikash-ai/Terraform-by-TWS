f9a634e4a4ad5b7fe6d1c556d2879a7001eb4490
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-09-01 17:45:57 +0530

    file add

--- ec2.tf
+++ ec2.tf
@@ -53,10 +53,10 @@ resource aws_security_group my_security_group{
 resource "aws_instance" "my-instance" {
     key_name = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_security_group.name]
-    instance_type = "t3.micro"
-    ami = "ami-0e5497a77ef21b5ac" # Ubuntu AMI (HVM), SSD Volume Type
+    instance_type = var.ec2_instance_type
+    ami = var.ec2_ami_id # Ubuntu AMI (HVM), SSD Volume Type
     root_block_device {
-        volume_size = 8
+        volume_size = var.ec2_root_storage_size
         volume_type = "gp3"
     }
     tags = {
