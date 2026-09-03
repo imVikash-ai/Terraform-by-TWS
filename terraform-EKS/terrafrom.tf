099e414ece5c1ecae2430c319cd70eb213f49249
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-09-03 15:23:34 +0530

    file add

---
+++ terraform-EKS/terrafrom.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-bucket-for-terraform-23456"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+    # dynamodb_table = "tws-junoon-state-table"
+    # use_lockfile = true
+  }
+}
